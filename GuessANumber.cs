75b16e20cc017b40a2a3aead6eba8c9b4cb0d0df
Diyan D <122938689+DidkoD2@users.noreply.github.com>
2023-01-23 17:45:25 +0200

    Added the game

---
+++ GuessANumber.cs
@@ -0,0 +1,36 @@
+﻿
+Random randomNumber = new Random();
+int computerNumber = randomNumber.Next(1, 101);
+
+while (true)
+{
+    Console.Write("Guess a number (1 - 100): ");
+    string playerInput = Console.ReadLine();
+    bool isValid = int.TryParse(playerInput, out int playerNumber);
+
+    if (isValid)
+    {
+        if (playerNumber < computerNumber)
+        {
+            Console.WriteLine("Too Low");
+        }
+        else if (playerNumber > computerNumber)
+        {
+            Console.WriteLine("Too High");
+        }
+        else
+        {
+            Console.WriteLine("You Guessed the number!");
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input");
+    }
+}
+
+
+
+
+
